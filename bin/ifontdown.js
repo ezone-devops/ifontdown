671a7ab192268a407f216c7e8f98b56986992632
lishangxi <lishangxi@ezone.work>
2022-07-11 15:40:05 +0800

    fix: update name

--- bin/ifontdown.js
+++ bin/ifontdown.js
@@ -5,15 +5,14 @@ const { Command } = require('commander');
 const program = new Command();
 
 const pkg = require('../package');
-const pkg2 = require('..');
 const Downloader = require('..');
 program
-  .name('ddi')
+  .name('ifontdown')
   .description('download iconfont file')
   .version(pkg.version);
 
 program.command('start')
-  .description('开始下载iconfont 文件')
+  .description('下载iconfont 文件')
 //   .argument('<string>', 'string to split')
   .option('--first', 'display just the first substring')
   .option('-o, --output <string>', 'separator character', ',')
